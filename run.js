63ef968d86be31cc5059eff0c5167d634771f1ab
genrum <gen.rumtcv@gmail.com>
2022-02-03 17:13:23 -0800

    implemented the switch case

--- run.js
+++ run.js
@@ -4,21 +4,29 @@ function Calc(oper, a, b) {
 
     if (isNotANumber || isOperatorUndefined) {
         return "error";
-    } else if (oper == "sum") {
-        return a + b;
-    } else if (oper == "multi") {
-        return a * b;
-    } else if (oper == "ded") {
-        return a - b;
-    } else if (oper == "div") {
-        return a / b;
-    } else if (oper == "rem") {
-        return a % b;
-    } else if (oper == "pow") {
-        return a ** b;
-    } else {
-        return "unknown operation";
+    } 
+    switch (oper) {
+        case "sum": 
+            return a + b;
+            break;
+        case "multi":
+            return a * b;
+            break;
+        case "ded":
+            return a - b;
+            break;
+        case "div":
+            return a / b;
+            break;
+        case "rem":
+            return a % b;
+            break;
+        case "pow":
+            return a ** b;
+            break;
+        default:
+            return "unknown operation";
+            break;
     }
-};
-
-console.log(Calc("div", 12, 1))
+}
+console.log(Calc("sum", 12, 1));
